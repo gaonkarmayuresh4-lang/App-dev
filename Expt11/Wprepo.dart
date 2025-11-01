be43bbb4aaa5ab02929310b4fd8dbb71554d8820
gaonkarmayuresh4-lang <gaonkarmayuresh4@gmail.com>
2025-11-01 16:20:54 +0530

    Change

---
+++ Expt11/Wprepo.dart
@@ -0,0 +1,19 @@
+import '../models/wallpaper.dart';
+
+import '../services/api_client.dart';
+
+
+
+class WallpaperRepository {
+
+  final ApiClient apiClient = ApiClient();
+
+
+
+  Future<List<Wallpaper>> getWallpapers() async {
+
+    return await apiClient.fetchWallpapers();
+
+  }
+
+}
